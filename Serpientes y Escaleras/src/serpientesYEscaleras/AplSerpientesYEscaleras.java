485069d7a90c242e0a011a2171275df82155ab20
Unknown <emanuel.l4168@gmail.com>
2018-11-17 21:25:07 -0700

    Creación de la clase NodoDBL
    Se creó la clase NodoDBL, que permite crear una lista doblemente ligada

--- Serpientes y Escaleras/src/serpientesYEscaleras/AplSerpientesYEscaleras.java
+++ Serpientes y Escaleras/src/serpientesYEscaleras/AplSerpientesYEscaleras.java
@@ -2,6 +2,18 @@ package serpientesYEscaleras;
 
 public class AplSerpientesYEscaleras {
 
+	private static class Casilla{
+		public int noCasilla;
+		public char tipoCasilla;
+		public int posiciones;
+		
+		public Casilla(int nc, char tc, int p) {
+			noCasilla = nc;
+			tipoCasilla = tc;
+			posiciones = p;
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
